7faa7e28205e078747ea56d10e0fa1aae133dcbe
pomorange <nemanjapipi@hotmail.com>
2019-06-18 03:06:39 +0200

    uradio Get i GetId

---
+++ MongoDb/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDb
+{
+    public class PersonModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressModel PrimaryAddress { get; set; }
+    }
+}
---
+++ MongoDb/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBook");
+            //PersonModel person = new PersonModel
+            //{
+            //    FirstName = "Dusan",
+            //    LastName = "Mihajlovic",
+            //    PrimaryAddress = new AddressModel
+            //    {
+            //        StreetAddress = "Petra Konjovica 12",
+            //        City = "Zabalj",
+            //        State = "Serbia",
+            //        ZipCode = "21230"
+            //    }
+            //};
+            //db.InsertRecord("Users", person);
+
+            //var recs = db.LoadRecords<PersonModel>("Users");
+
+            //foreach(var rec in recs)
+            //{
+            //    Console.WriteLine($"{rec.Id}: {rec.FirstName} {rec.LastName}");
+
+            //    if(rec.PrimaryAddress != null)
+            //    {
+            //        Console.WriteLine(rec.PrimaryAddress.City);
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            var oneRec = db.LoadRecordById<PersonModel>("Users", new Guid("d326ee5c-a05e-4b95-91f4-50e4aba443be"));
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
